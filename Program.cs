bec9a9d77367963a734e3a5caa99b5f42fff1a7e
Mikhail Shelomentsev <smv_win@hotmail.com>
2020-04-30 18:51:01 +0300

    Another simple feature added

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ namespace SampleBookingApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi, i'm booking app.");
-            Console.WriteLine("I'm from SuperFeature branch!");
+            Console.WriteLine("Hi, i'm booking app.");            
             Console.WriteLine("From master with love ^^~");
             Console.ReadKey(true);
         }
