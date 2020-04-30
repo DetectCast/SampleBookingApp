f4dfa12e7e19ce7fff89a3c9450a42b7e49269f8
Mikhail Shelomentsev <smv_win@hotmail.com>
2020-04-30 18:15:19 +0300

    Added the SuperFeature

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace SampleBookingApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hi, i'm booking app.");
+            Console.WriteLine("I'm from SuperFeature branch!");
             Console.ReadKey(true);
         }
     }
