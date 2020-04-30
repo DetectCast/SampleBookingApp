63cf5e4261f02c99d9e00dd5a2cffe4733fecd06
Mikhail Shelomentsev <smv_win@hotmail.com>
2020-04-30 16:51:18 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SampleBookingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, i'm booking app.");
+            Console.ReadKey(true);
+        }
+    }
+}
